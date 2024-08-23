e3ddf01c479ac19360bfd5f8fad2c72dc912ed28
roniislam999 <mdronii961@gmail.com>
2024-08-23 22:23:34 +0600

    project topic explore part added

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,8 +1,14 @@
+import { Link } from "react-router-dom"
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+   <h1>About</h1>
+   <div className="text-center">
+      <Link to={'https://docs.google.com/document/d/1peBh_TvJYPwnaBi0rfdCKvrNrk6-3hrCZ5_DNuHG9lY/edit'} target="blank"><button className="bg-blue-600 text-white py-2 px-4 cursor-pointer rounded-sm">Project Topic Explore ...</button></Link>
+   </div>
+    </div>
   )
 }
 
